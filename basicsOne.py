3a8df61819bf398c954973765bd7ae1865f92975
Ratul Paul <RATULP@amdocs.com>
2018-09-26 08:48:26 +0530

    first commit

---
+++ basicsOne.py
@@ -0,0 +1,20 @@
+# print("Hello World!")
+
+
+score = "HelloWorld!"
+
+print(score[0])
+print(score[2:6])
+print(score[2:])
+
+# print(score[2:])
+
+mylist = ['hulk', 3.87, 'spiderman', 4]
+
+print(mylist[1:3])
+
+mytuple =('text', 4.8)  -> cannot be updated
+
+mydict={'name':'superman','age':40}  --> key value pair
+
+print(mydict.keys())
